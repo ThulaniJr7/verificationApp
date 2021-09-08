fbe764a140a02619816ef9be5a5b1e09da048193
The Good People Data Company <78204504+TGPDCAdmin@users.noreply.github.com>
2021-09-08 14:12:38 +0200

    Updated!

---
+++ app/src/main/java/com/example/verificationapp/MainActivityProvApproved.java
@@ -0,0 +1,58 @@
+package com.example.verificationapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivityProvApproved extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener
+{
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_prov_approved);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_logos_02);
+
+    }
+
+    public void newApplicationForm(View view)
+    {
+
+//      Changing to a new activity
+        startActivity(new Intent(MainActivityProvApproved.this, MainActivity2.class));
+        finish();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        int id = item.getItemId();
+
+        switch (id)
+        {
+            case R.id.navigation_home:
+                Intent intent = new Intent(MainActivityProvApproved.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+        }
+
+        return false;
+    }
+
+}
